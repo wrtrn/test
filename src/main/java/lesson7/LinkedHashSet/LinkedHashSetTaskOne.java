f6af131820d1726f1b0c7c59c8ef2b00537d3f96
oa <a@ggsel.net>
2025-07-31 00:57:27 +0300

    Lesson 7

---
+++ src/main/java/lesson7/LinkedHashSet/LinkedHashSetTaskOne.java
@@ -0,0 +1,20 @@
+package lesson7.LinkedHashSet;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetTaskOne {
+    private LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+
+    public LinkedHashSetTaskOne() {
+        linkedHashSet.add("fadsasdas");
+        linkedHashSet.add("vcxvxcvxs");
+        linkedHashSet.add("kluilujkl");
+        linkedHashSet.add("gfdvcx");
+        linkedHashSet.add("lsdkfXXX");
+    }
+
+    public void printElementsInCorrectOrder()
+    {
+        linkedHashSet.forEach(System.out::println);
+    }
+}
