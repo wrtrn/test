6a3c2c22b1a4329538499e3d6c7389d765aecda4
oa <a@ggsel.net>
2025-10-31 22:35:12 +0200

    junior tests added

---
+++ src/test/java/nBankTests/fistPart/LoginUserTest.java
@@ -0,0 +1,76 @@
+package nBankTests.fistPart;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class LoginUserTest {
+    @BeforeAll
+    public static void setupRestAssured() {
+        RestAssured.filters(
+                List.of(new RequestLoggingFilter(),
+                        new ResponseLoggingFilter()));
+    }
+
+    @Test
+    public void adminCanGenerateAuthTokenTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("""
+                        {
+                          "username": "admin",
+                          "password": "admin"
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=");
+    }
+
+    @Test
+    public void userCanGenerateAuthTokenTest() {
+        // создание пользователя
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body("""
+                        {
+                          "username": "kate2000",
+                          "password": "Kate2000#",
+                          "role": "USER"
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("""
+                        {
+                          "username": "kate2000",
+                          "password": "Kate2000#"
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .header("Authorization", Matchers.notNullValue());
+    }
+}
---
+++ src/test/java/nBankTests/secondPart/TransferTest.java
@@ -0,0 +1,202 @@
+package nBankTests.secondPart;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import nBankTests.BaseApiTest;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.restassured.RestAssured.given;
+
+public class TransferTest extends BaseApiTest {
+
+    @Test
+    public void transferMoneyFromEmptyAccount() {
+        int firstAccountNumber = createAccount(getFirstUserToken());
+        int secondAccountNumber = createAccount(getFirstUserToken());
+
+        String requestBody = String.format(
+                """
+                                {
+                                  "senderAccountId": %s,
+                                  "receiverAccountId": %s,
+                                  "amount": 50
+                                }
+                        """, firstAccountNumber, secondAccountNumber);
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", getFirstUserToken())
+                .body(requestBody)
+                .when()
+                .post("/api/v1/accounts/transfer")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void userCanTransferBetweenHisOwnAccounts() {
+        int firstAccountNumber = createAccount(getFirstUserToken());
+        int secondAccountNumber = createAccount(getFirstUserToken());
+
+        addMoneyToAccount(getFirstUserToken(), firstAccountNumber, 500);
+
+        String requestBody = String.format(
+                """
+                                {
+                                  "senderAccountId": %s,
+                                  "receiverAccountId": %s,
+                                  "amount": 50
+                                }
+                        """, firstAccountNumber, secondAccountNumber);
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", getFirstUserToken())
+                .body(requestBody)
+                .when()
+                .post("/api/v1/accounts/transfer")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+
+        Double firstAccountBalance = getAccountBalance(getFirstUserToken(), firstAccountNumber);
+        Double secondAccountBalance = getAccountBalance(getFirstUserToken(), secondAccountNumber);
+
+        Assertions.assertEquals(450, firstAccountBalance);
+        Assertions.assertEquals(50, secondAccountBalance);
+    }
+
+    @Test
+    public void userSeesOnlyHisAccounts() {
+        int secondUserAccountNumber = createAccount(getSecondUserToken());
+
+        JsonPath firstUserAccountsJson = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", getFirstUserToken())
+                .when()
+                .get("/api/v1/customer/accounts")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .jsonPath();
+
+        Assertions.assertThrows(NullPointerException.class, () -> firstUserAccountsJson.getInt("find { it.id == " + secondUserAccountNumber + " }?.id"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10000, 9999, 1})
+    public void userCanTransferToAnotherUser(int amount) {
+        int firstUserAccountNumber = createAccount(getFirstUserToken());
+        int secondUserAccountNumber = createAccount(getSecondUserToken());
+
+        addMoneyToAccount(getFirstUserToken(), firstUserAccountNumber, 5000);
+        addMoneyToAccount(getFirstUserToken(), firstUserAccountNumber, 5000);
+
+        String requestBody = String.format(
+                """
+                                {
+                                  "senderAccountId": %s,
+                                  "receiverAccountId": %s,
+                                  "amount": %s
+                                }
+                        """, firstUserAccountNumber, secondUserAccountNumber, amount);
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", getFirstUserToken())
+                .body(requestBody)
+                .when()
+                .post("/api/v1/accounts/transfer")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+
+        Double firstAccountBalance = getAccountBalance(getFirstUserToken(), firstUserAccountNumber);
+        Double secondAccountBalance = getAccountBalance(getSecondUserToken(), secondUserAccountNumber);
+
+        Assertions.assertEquals(10000 - amount, firstAccountBalance);
+        Assertions.assertEquals(amount, secondAccountBalance);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10001, 0, -400})
+    public void userCanTransferToAnotherUserInvalidAmountOfMoney(int amount) {
+        int firstUserAccountNumber = createAccount(getFirstUserToken());
+        int secondUserAccountNumber = createAccount(getSecondUserToken());
+
+        addMoneyToAccount(getFirstUserToken(), firstUserAccountNumber, 5000);
+        addMoneyToAccount(getFirstUserToken(), firstUserAccountNumber, 5000);
+        addMoneyToAccount(getFirstUserToken(), firstUserAccountNumber, 5000);
+
+        String requestBody = String.format(
+                """
+                                {
+                                  "senderAccountId": %s,
+                                  "receiverAccountId": %s,
+                                  "amount": %s
+                                }
+                        """, firstUserAccountNumber, secondUserAccountNumber, amount);
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", getFirstUserToken())
+                .body(requestBody)
+                .when()
+                .post("/api/v1/accounts/transfer")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+
+
+        Double firstAccountBalance = getAccountBalance(getFirstUserToken(), firstUserAccountNumber);
+        Double secondAccountBalance = getAccountBalance(getSecondUserToken(), secondUserAccountNumber);
+
+        Assertions.assertEquals(15000, firstAccountBalance);
+        Assertions.assertEquals(0, secondAccountBalance);
+    }
+
+    @Test
+    public void transferAmountGreaterThanSenderBalance() {
+        int firstUserAccountNumber = createAccount(getFirstUserToken());
+        int secondUserAccountNumber = createAccount(getSecondUserToken());
+
+        addMoneyToAccount(getFirstUserToken(), firstUserAccountNumber, 5000);
+
+        String requestBody = String.format(
+                """
+                                {
+                                  "senderAccountId": %s,
+                                  "receiverAccountId": %s,
+                                  "amount": %s
+                                }
+                        """, firstUserAccountNumber, secondUserAccountNumber, 5050);
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", getFirstUserToken())
+                .body(requestBody)
+                .when()
+                .post("/api/v1/accounts/transfer")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+
+        Double firstAccountBalance = getAccountBalance(getFirstUserToken(), firstUserAccountNumber);
+        Double secondAccountBalance = getAccountBalance(getSecondUserToken(), secondUserAccountNumber);
+
+        Assertions.assertEquals(5000, firstAccountBalance);
+        Assertions.assertEquals(0, secondAccountBalance);
+    }
+}
