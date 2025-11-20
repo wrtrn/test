a47199a016f64eee24e0e6df6d661c670ad612e2
oa <a@ggsel.net>
2025-11-20 10:56:16 +0200

    unnecessary waitings removal

--- src/test/java/nbank/ui/secondPart/NicknameTest.java
+++ src/test/java/nbank/ui/secondPart/NicknameTest.java
@@ -1,6 +1,8 @@
 package nbank.ui.secondPart;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.conditions.Visible;
 import nbank.api.models.CreateUserRequest;
 import nbank.api.requests.steps.AdminSteps;
 import nbank.ui.BaseUiTest;
@@ -26,22 +28,20 @@ public class NicknameTest extends BaseUiTest {
         CreateUserRequest user = AdminSteps.createUser();
         authAsUser(user);
 
-        userDashboard.open();
-        Selenide.sleep(1000);
+        userDashboard.open().getRightCornerNameText().shouldBe(Condition.visible);
 
         String welcomeText = userDashboard.getWelcomeText().getText();
         String rightCornerNameText = userDashboard.getRightCornerNameText().getText();
         Assertions.assertEquals("Welcome, noname!", welcomeText);
         Assertions.assertEquals("Noname", rightCornerNameText);
 
+
         userDashboard.openEditProfile();
         Selenide.sleep(1000);
-
         new EditProfile().setNewName(newName).checkAlertMessageAndAccept(NAME_UPDATED_SUCCESSFULLY.getMessage());
 
-        userDashboard.open();
+        userDashboard.open().getRightCornerNameText().shouldBe(Condition.visible);
 
-        Selenide.sleep(1000);
         welcomeText = userDashboard.createNewAccount().getWelcomeText().getText();
         rightCornerNameText = userDashboard.getRightCornerNameText().getText();
 
@@ -65,8 +65,7 @@ public class NicknameTest extends BaseUiTest {
 
         editProfile.setNewName(newName).checkAlertMessageAndAccept(PLEASE_ENTER_A_VALID_NAME.getMessage());
 
-        userDashboard.open();
-        Selenide.sleep(1000);
+        userDashboard.open().getRightCornerNameText().shouldBe(Condition.visible);
 
         String welcomeText = userDashboard.getWelcomeText().getText();
         String rightCornerNameText = userDashboard.getRightCornerNameText().getText();
